dd989f04743647cbfeda3142d55306882671a8f5
gediminas.stanaitis <gvaliooo@gmail.com>
2014-11-26 23:39:46 +0200

    home page updates

--- header.php
+++ header.php
@@ -1,112 +1,53 @@
 <!DOCTYPE html>
 
 <!--[if lte IE 7]>
-
-<html class="ie7 page-home"><![endif]-->
-
+<html class="ie7"><![endif]-->
 <!--[if IE 8]>
-
-<html class="ie8 page-home"><![endif]-->
-
+<html class="ie8"><![endif]-->
 <!--[if IE 9]>
-
-<html class="ie9 page-home"><![endif]-->
-
+<html class="ie9"><![endif]-->
 <!--[if !IE]><!-->
-
-<html class="page-home"><!--<![endif]-->
+<html><!--<![endif]-->
 
 <head>
 
     <meta charset="utf-8">
-
-
-
     <title>Title</title>
-
     <meta name="description" content="">
-
     <meta name="keywords" content="">
-
-
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="format-detection" content="telephone=no">
-
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,700,700italic|Great+Vibes|Lora:400,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-
     <link type="text/css" rel="stylesheet" href="<?php echo get_template_start(); ?>content/styles/css/styles.css">
 
-
-
     <!--[if lt IE 9]>
-
-    <script src="/scripts/html5shim.js" type="text/javascript"></script>
-
+        <script src="<?php echo get_template_start(); ?>scripts/html5shim.js" type="text/javascript"></script>
     <![endif]-->
-
-
 
     <script type="text/javascript">
 
         (function () {
-
             'use strict';
-
             var el, head;
-
             el = document.createElement('script');
-
             el.type = 'text/javascript';
-
             el.async = true;
-
             el.setAttribute("data-main","<?php echo get_template_start(); ?>scripts/main");
-
             el.src = '<?php echo get_template_start(); ?>scripts/require.js';
-
             head = document.getElementsByTagName('script')[0];
-
             head.parentNode.insertBefore(el, head);
-
         }());
 
     </script>
 
 </head>
 
-	<body>
-
-        <?php if (is_front_page()) { ?>
-            <div class="page-header home">
-                <div class="page-frame clearfix">
-                    <a href="#" class="logo">
-                        <img src="<?php echo get_template_start(); ?>content/images/logo.png" alt="Asta Uskaite">
-                    </a>
-                    <?php include("home-menu.php"); ?>
-                </div>
-            </div>
-            <section class="page-section promo" id="promo">
-                <div id="promo-slide">
-                    <div class="slide s01"></div>
-                    <div class="slide s02"></div>
-                    <div class="slide s03"></div>
-                    <div class="slide s04"></div>
-                    <div class="slide s05"></div>
-                </div>
-            </section>
-        <?php } ?>
-
-        <header class="page-header <?php if (is_front_page()) { ?> js-header"<?php } ?>">
-
-                <div class="page-frame clearfix">
-
-                <a href="#" class="logo">
-                    <img src="<?php echo get_template_start(); ?>content/images/logo-black.png" alt="Asta Uskaite">
-                </a>
-                <?php include("home-menu.php"); ?>
-
-                </div>
-
-        </header>
+<body>
+    <header class="page-header<?php if (is_front_page()) { ?> home<?php } ?>">
+        <div class="page-frame clearfix">
+            <a href="#" class="logo">
+                <img src="<?php echo get_template_start(); ?>content/images/logo-black.png" alt="Asta Uskaite">
+            </a>
+            <?php include("home-menu.php"); ?>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php get_header(); ?>
 
+<section class="page-section promo" id="promo">
+    <div id="promo-slide">
+        <div class="slide s01"></div>
+        <div class="slide s02"></div>
+        <div class="slide s03"></div>
+        <div class="slide s04"></div>
+        <div class="slide s05"></div>
+    </div>
+</section>
+
 <section class="page-section about" id="about">
 
     <div class="page-frame clearfix">
-
-
-
         <h1>
             <?php
                 echo __(get_the_title(39));
@@ -13,207 +20,27 @@
         </h1>
 
         <?php
-
             $post_about_me = get_post(39);
             echo __($post_about_me->post_content);
-
         ?>
 
         <div class="clearfix">
 
             <figure class="img-frame">
-
                 <img src="http://lorempixel.com/g/211/252/fashion/" alt="Asta Uskaite">
-
             </figure>
 
             <figure class="img-frame">
-
                 <img src="http://lorempixel.com/g/211/252/fashion/" alt="Asta Uskaite">
-
             </figure>
 
             <figure class="img-frame">
-
                 <img src="http://lorempixel.com/g/211/252/fashion/" alt="Asta Uskaite">
-
             </figure>
 
         </div>
-
-
-
     </div>
 
 </section>
-
-<section class="page-section gallery" id="gallery">
-
-    <div class="page-frame">
-
-
-        <h1>
-            <?php
-                echo __(get_the_title(41));
-            ?>
-        </h1>
-
-        <div class="catalog-list">
-
-            <div class="catalog papuosalai">
-
-                <h3>Kaklo papuošalai</h3>
-
-                <img src="http://lorempixel.com/960/275/nature/" alt="">
-
-                <a href="/gallery.html">žiūreti galerija</a>
-
-            </div>
-
-            <div class="catalog sages dir-right">
-
-                <h3>Sagės</h3>
-
-                <img src="http://lorempixel.com/960/275/nature/" alt="">
-
-                <a href="/gallery.html">žiūreti galerija</a>
-
-            </div>
-
-            <div class="catalog apyrankes">
-
-                <h3>Apyrankės</h3>
-
-                <img src="http://lorempixel.com/960/275/nature/" alt="">
-
-                <a href="/gallery.html">žiūreti galerija</a>
-
-            </div>
-
-            <div class="catalog auskarai dir-right">
-
-                <h3>Auskarai</h3>
-
-                <img src="http://lorempixel.com/960/275/nature/" alt="">
-
-                <a href="/gallery.html">žiūreti galerija</a>
-
-            </div>
-
-            <div class="catalog aksesuarai">
-
-                <h3>Galvos aksesuarai</h3>
-
-                <img src="http://lorempixel.com/960/275/nature/" alt="">
-
-                <a href="/gallery.html">žiūreti galerija</a>
-
-            </div>
-
-        </div>
-
-
-
-    </div>
-
-</section>
-
-<section class="page-section contact" id="contact">
-
-    <div id="contact-map"></div>
-
-    <div class="page-frame">
-
-        <div class="contact-box">
-
-            <div class="contact-header">
-
-                <dl>
-
-                    <dt><img src="<?php echo get_template_start(); ?>content/images/icn-phone.png" alt="">tel.:</dt>
-
-                    <dd><?php echo __(get_field('phone_con', 44)); ?></dd>
-
-                    <dt><img src="<?php echo get_template_start(); ?>content/images/icn-address.png" alt="">adresas:</dt>
-
-                    <dd><?php echo __(get_field('address_con', 44)); ?></dd>
-
-                    <dt><img src="<?php echo get_template_start(); ?>content/images/icn-facebook.png" alt="">facebook:</dt>
-
-                    <dd><?php echo __(get_field('facebook_con', 44)); ?></dd>
-
-                </dl>
-
-            </div>
-
-            <div class="contact-form">
-
-                <a class="link-toggle js-toggle">&ndash;</a>
-
-                <form method="post" action="" class="js-contactform">
-
-                    <div class="two-columns clearfix">
-
-                        <div class="form-row">
-
-                            <label for="fname">vardas</label>
-
-                            <input type="text" id="fname" name="fname" class="textfield required js-field" placeholder="vardas" title="&nbsp;">
-
-                        </div>
-
-                        <div class="form-row">
-
-                            <label for="lname">pavardė</label>
-
-                            <input type="text" id="lname" name="lname" class="textfield js-field" placeholder="pavardė" title="&nbsp;">
-
-                        </div>
-
-                    </div>
-                    <div class="two-columns clearfix">
-
-                        <div class="form-row">
-
-                            <label for="email">el. paštas</label>
-
-                            <input type="email" id="email" name="email" class="textfield required js-field" placeholder="el. paštas" title="&nbsp;">
-
-                        </div>
-
-                        <div class="form-row">
-
-                            <label for="phone">telefonas</label>
-
-                            <input type="text" id="phone" name="phone" class="textfield required js-field" placeholder="telefonas" title="&nbsp;">
-
-                        </div>
-
-                    </div>
-                    <div class="form-row textarea-row">
-
-                        <label for="textarea">žinutė</label>
-
-                        <textarea id="textarea" class="textarea required js-field" name="zinute" placeholder="žinutė" title="&nbsp;"></textarea>
-
-                    </div>
-                    <div class="form-row">
-
-                        <button class="btn">siųsti žinutę</button>
-
-                    </div>
-
-                </form>
-
-
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
-
 
 <?php get_footer(); ?>
