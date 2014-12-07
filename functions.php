2258e4a884e5ba694019ac0e60ca5ab193a62ed3
Arturas <arturas.aleksandrov@gmail.com>
2014-12-07 16:09:40 +0200

    Added /staging/ path to menu links for staging server.

--- functions.php
+++ functions.php
@@ -490,7 +490,7 @@ class ik_walker extends Walker_Nav_Menu{
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
         $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $this->__($item->url)        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="/staging/'   . esc_attr( $this->__($item->url)        ) .'"' : '';
 
         $item_output = $args->before;
         $item_output .= '<a'. $attributes .'>';
