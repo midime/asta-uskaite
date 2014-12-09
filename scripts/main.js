2659838e2c64a8defda4abbc5bdae17cf125a2e7
Gediminas Stanaitis <gedas@devbridge.com>
2014-12-09 23:00:52 +0200

    fix main.js issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
-﻿/*jslint regexp: true, nomen: true, sloppy: true *//*global require, applicationConfig, window, applicationConfig */require.config({    baseUrl: '/staging/wp-content/themes/asta-uskaite/scripts',    paths: {        jquery: 'libs/jquery-1.8.3.min',        site: 'modules/site',        validation: 'plugins/jquery.validate',        jqueryEasing:'plugins/jquery.easing.1.3',        bxslider:'plugins/jquery.bxslider.min',        googlemaps: 'plugins/gmaps',        async: 'libs/async'    },    shim: {        site: {            deps: ['jquery']        },        validation: {            deps: ['jquery']        },        jqueryEasing:{            deps:['jquery']        },        bxslider:{            deps:['jquery']        }    }});require(['jquery', 'site', 'bxslider', 'jqueryEasing', 'async', 'googlemaps', 'validation'], function ($, site) {    $(function(){        site.init();    });});
+﻿/*jslint regexp: true, nomen: true, sloppy: true */
+
+/*global require, applicationConfig, window, applicationConfig */
+
+require.config({
+
+    baseUrl: '/staging/wp-content/themes/asta-uskaite/scripts',
+    paths: {
+        jquery: 'libs/jquery-1.8.3.min',
+        site: 'modules/site',
+        validation: 'plugins/jquery.validate',
+        jqueryEasing:'plugins/jquery.easing.1.3',
+        bxslider:'plugins/jquery.bxslider.min',
+        googlemaps: 'plugins/gmaps',
+        async: 'libs/async'
+
+    },
+
+    shim: {
+        site: {
+            deps: ['jquery']
+        },
+        validation: {
+            deps: ['jquery']
+        },
+        jqueryEasing:{
+            deps:['jquery']
+        },
+        bxslider:{
+            deps:['jquery']
+        }
+    }
+
+});
+
+require(['jquery', 'site', 'bxslider', 'jqueryEasing', 'async', 'googlemaps', 'validation'], function ($, site) {
+    $(function(){
+        site.init();
+    });
+});
