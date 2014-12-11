d382e9684d484a308a9b554240d1ee45f6da7847
Arturas <arturas.aleksandrov@gmail.com>
2014-12-11 22:12:13 +0200

    Fixed footer translations.

--- html-footer.php
+++ html-footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer page-frame clearfix">
     <div class="contacts">
         <span class="tel"><?php echo __(get_field('phone_con', 44)); ?></span>
-        <span class="address">Adresas: <?php echo __(get_field('address_con', 44)); ?></span>
+        <span class="address"><?php echo getQVertimas('footer_address'); ?>: <?php echo __(get_field('address_con', 44)); ?></span>
         <a class="facebook" href=""><?php echo __(get_field('facebook_con', 44)); ?></a>
     </div>
 </footer>
