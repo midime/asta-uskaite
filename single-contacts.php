18e57264cba13afc6ce0ef704fd05e2f1b9156ad
Arturas <arturas.aleksandrov@gmail.com>
2014-12-11 21:10:11 +0200

    Fixed contact form page and added eol lf to all files.

--- single-contacts.php
+++ single-contacts.php
@@ -5,10 +5,15 @@
 
     <h1><?php echo do_shortcode(get_the_title(get_the_ID())); ?></h1>
     <div class="contacts centered">
-        <span class="tel"><?php echo get_field('phone_con', 16); ?></span>
-        <span class="address"><a href="" class="js-contact"><?php echo get_field('address_con', 16); ?></a></span>
-        <span class="facebook"><?php echo get_field('facebook_con', 16); ?></span>
+        <span class="tel"><?php echo get_field('phone_con', 44); ?></span>
+        <span class="address"><a href="" class="js-contact"><?php echo get_field('address_con', 44); ?></a></span>
+        <span class="facebook"><?php echo get_field('facebook_con', 44); ?></span>
     </div>
+    <?php
+        //$ct_shortcode = translate_phrases_of_forms(do_shortcode('[contact-form-7 id="77" title="Be pavadinimo"]'));
+        $ct_shortcode = translate_phrases_of_forms(do_shortcode('[contact-form-7 id="81" title="Be pavadinimo"]'));
+        echo $ct_shortcode;
+    ?>
     <form action="" method="post">
         <div class="field-wrapper">
             <input type="text" id="name1" name="name" class="field-text required" value="" placeholder="Vardas">
