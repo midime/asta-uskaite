c37c8586fbfd30effa095bdf8e966e3d3b702ac0
Gediminas Stanaitis <gedas@devbridge.com>
2014-12-21 20:06:59 +0200

    responsive header

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
 </head>
 
 <body<?php if (is_front_page()) { ?> class="home"<?php } ?>>
-    <header class="page-header<?php if (is_front_page()) { ?> home<?php } ?> opened">
+    <header class="page-header<?php if (is_front_page()) { ?> home<?php } ?>">
         <div class="page-frame clearfix">
             <a href="/<?php echo WEBFOLDER; ?>/" class="logo">
                 <?php if (is_front_page()) { ?>
